c3550cfe65ac99451be919b841e71e729b467d05
Nmwai <nmwai01@gmail.com>
2022-07-04 07:27:49 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+include <stdio.h>
+
+/**
+ * Main - Entry point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+
+}

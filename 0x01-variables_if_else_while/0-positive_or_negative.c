99e10da9fbc6235589d8eaae20d1aafc7dd0d373
Nmwai <nmwai01@gmail.com>
2022-07-25 06:01:32 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,16 +10,16 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-	printf("is positive\n", n);
+	("%d,is positive\n", n);
 	}
 	else
 	(n = 0)
 	{
-	printf("is zero\n", n);
+	("%d, is zero\n", n);
 	}
 	if (n < 0)
 	{
-	printf("is negative\n", n);
+	("%d, is negative\n", n);
 	}
 	return (0);
 }

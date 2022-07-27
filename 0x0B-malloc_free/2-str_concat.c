3eeb137128259d75761cb3eebaac62f91bd6c911
Nmwai <nmwai01@gmail.com>
2022-07-27 15:01:05 -0700

    a function that concatenates two strings.

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,39 +1,45 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 /**
- * str_concat - get ends of input and add together for size
- * @s1: input one to concat
- * @s2: input two to concat
- * Return: concat of s1 and s2
+ * _strlen - returns the length of a string.
+ * @s: pointer to String
+ * Return: Nothing
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ * str_concat - function that concatenates two strings.
+ * @s1: char to concatenate
+ * @s2: char to concatenate
+ *
+ * Return: pointer to char
  */
 char *str_concat(char *s1, char *s2)
 {
-	int end1, end2, i = 0;
-	char *array;
+	char *str, lenght = 0, lenght1 = 0, lenght2 = 0, i;
 
-	if (s1 == NULL || s2 == NULL)
-		s1 = s2 = "";
-	for (end1 = 0; end1 <= *s1; end1++)
-	{
-	}
-	for (end2 = 0; end2 <= *s2; end2++)
-	{
-	}
-	array = malloc(sizeof(char) * (end1 + end2 + 1));
-	if (array == NULL)
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	lenght1 = _strlen(s1);
+	lenght2 = _strlen(s2);
+	lenght = lenght1 + _strlen(s2) + 1;
+	str = malloc(lenght * sizeof(char));
+	if (str == NULL)
 		return (NULL);
-	while (*s1)
-	{
-		array[i] = *s1;
-		i++;
-		s1++;
-	}
-	while (*s2)
-	{
-		array[i] = *s2;
-		i++;
-		s2++;
-	}
-	return (array);
+	for (i = 0; i < lenght1; i++)
+		*(str + i) = *(s1 + i);
+	for (i = 0; i <= lenght2; i++)
+		*(str + i + lenght1) = *(s2 + i);
+	return (str);
 }

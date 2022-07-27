d9d524dbfa56b743f65430ff6069409b7646af97
Nmwai <nmwai01@gmail.com>
2022-07-27 13:23:38 -0700

    Static Libraries

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -16,13 +16,12 @@ int _strlen(char *s)
 }
 /**
  * _puts - prints to string
- * @str: String to print
+ * @str: Strin to print
  * Return: Nothing
  */
 void _puts(char *str)
 {
 	int i = 0, strln = 0;
-
 	strln = _strlen(str);
 	for ( ; i <= (strln - 1); i++)
 	{
